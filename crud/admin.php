cd5540c9383fe0d71ad217470359b57820485655
Joao-Guilherme-GOliveira <joaoguilhermegrisostomo@outlook.com>
2026-06-26 16:03:16 -0300

    Melhorado o calculo de IMC

--- crud/admin.php
+++ crud/admin.php
@@ -20,7 +20,16 @@ if ($_SESSION['tipo_usuario'] != "admin") {
     <link rel="stylesheet" href="../css/crud.css">
 </head>
 <body>
-
+<header>
+<div class="logo-area">
+        <img src="../imagens/logobranco.png" alt="Logo">
+        <h2>Sistema Academia</h2>
+    </div>
+<nav class="navbar">
+        <a href="../index.php">Início</a>
+        <a href="../sair.php" class="sair">Sair</a>
+    </nav>
+</header>
 <div class="container">
 
 <h1>Painel de Admins</h1>
