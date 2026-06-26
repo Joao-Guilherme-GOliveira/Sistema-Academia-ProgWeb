3adfffc8c0b0f703553356235e1de56e0299edf1
Joao-Guilherme-GOliveira <joaoguilhermegrisostomo@outlook.com>
2026-06-25 22:12:06 -0300

    Navbar Adicionada

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ $nome = $_SESSION['nome'];
     <meta charset="UTF-8">
     <title>Sistema Academia</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/navbar.css">
 </head>
 <body>
 
@@ -24,7 +25,12 @@ $nome = $_SESSION['nome'];
         <h2>Sistema Academia</h2>
     </div>
 
-    <a href="sair.php" class="sair">Sair</a>
+     <nav class="navbar">
+        <a href="index.php">Início</a>
+        <a href="professores.php">Instrutores</a>
+        <a href="imc.php">Dieta Personalizada</a>
+        <a href="sair.php" class="sair">Sair</a>
+    </nav>
 </header>
 
 <main>
